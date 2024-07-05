cddc0966c613cf7760e070acff3846976a37c9f6
Nick Connolly <47530082+nconnolly1@users.noreply.github.com>
2024-07-05 20:39:08 +0100

    Save x16 and x17 during intercept

--- src/detours_arm64.asm
+++ src/detours_arm64.asm
@@ -46,11 +46,12 @@
 |_GetIntercept|	DCQ	DdkGetIntercept
 
 |_DdkWrapperMain|
-	stp			x0,x1,[sp,#-0x50]!
+	stp			x0,x1,[sp,#-0x60]!
 	stp			x2,x3,[sp,#0x10]
 	stp			x4,x5,[sp,#0x20]
 	stp			x6,x7,[sp,#0x30]
-	stp         x8,x9,[sp,#0x40]
+	stp         x8,x16,[sp,#0x40]
+	stp			x17,x9,[sp,#0x50]
 
 ;;  Call DdkGetIntercept
 
@@ -60,11 +61,12 @@
 	blr			x9
 	mov			x9,x0
 
-	ldp			x8,lr,[sp,#0x40]
+	ldp			x17,lr,[sp,#0x50]
+	ldp			x8,x16,[sp,#0x40]
 	ldp			x6,x7,[sp,#0x30]
 	ldp			x4,x5,[sp,#0x20]
 	ldp			x2,x3,[sp,#0x10]
-	ldp			x0,x1,[sp],#0x50
+	ldp			x0,x1,[sp],#0x60
 	br			x9	
 
 	ENDP	; |_DdkWrapper|
